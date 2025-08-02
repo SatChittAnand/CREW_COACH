0fd12af4920126d6d7b4785ded056e9399c012fc
SATYANARAYAN MOHANTY <satyanarayanmohanty177@gmail.com>
2025-08-02 16:35:05 +0530

    agents to be added

--- agents/job_market_scanner.py
+++
@@ -1,8 +0,0 @@
-from crewai import Agent
-
-job_market_scanner = Agent(
-    role='Job Market Scanner',
-    goal='Analyze current job market trends and popular roles in demand',
-    backstory='A job market analyst scanning global trends and in-demand technologies',
-    verbose=True
-)
